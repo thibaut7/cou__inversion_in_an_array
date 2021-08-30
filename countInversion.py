30943fe6fd2abd2d241dd746139e225155999e84
thibaut <86150292+thibaut7@users.noreply.github.com>
2021-08-30 09:00:08 +0000

    Add files via upload
    Algorithms specialisation  Programming assignment#2
    This file contains all of the 100,000 integers between 1 and 100,000 (inclusive) in some order, with no integer repeated.
    
     Your task is to compute the number of inversions in the file given, where the ithi^{th}ith row of the file indicates the ithi^{th}ith entry of an array.
    
      Because of the large size of this array, you should implement the fast divide-and-conquer algorithm covered in the video lectures.
    
    The numeric answer for the given input file should be typed in the space below.
    
    So if your answer is 1198233847, then just type 1198233847 in the space provided without any space / commas / any other punctuation marks. You can make up to 5 attempts, and we'll use the best one for grading.
    
    (We do not require you to submit your code, so feel free to use any programming language you want --- just type the final numeric answer in the following space.)
    
    [TIP: before submitting, first test the correctness of your program on some small test files or your own devising.  Then post your best test cases to the discussion forums to help your fellow students!]

---
+++ countInversion.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 27 09:18:19 2021
+
+@author: thibaut
+
+Inversions: num of (i, j) in A where i<j and A[i]>A[j]"""
+
+
+def sortAndCountInv(A):
+    if len(A)<=1:
+        return A, 0
+    else:
+        mid = len(A)//2
+        C = A[:mid]
+        D = A[mid:]
+# Recursively call sortAndCountInv on B and C
+        C, leftInv = sortAndCountInv(C)
+        D, rightInv = sortAndCountInv(D)
+        B, splitInv = mergeAndCountsplitInv(C, D)
+        return B, leftInv + rightInv + splitInv
+    
+def mergeAndCountsplitInv(C, D):
+    i=0
+    j=0
+    k=0
+    splitInv = 0
+    B = []
+    while i < len(C) and j < len(D):
+        if C[i] < D[j]:
+           B.append(C[i]) 
+           i+=1
+        else:
+            B.append(D[j])
+            j+=1
+# B is sorted then if D[j] is less than C[i] it'll be less than the len(C)-i elements
+            splitInv = splitInv +(len(C) - i) 
+        k+=1
+    while i < len(C):
+        B.append(C[i])
+        i+=1
+        k+=1
+    while j < len(D):
+        B.append(D[j])
+        j+=1
+        k+=1
+    return B, splitInv
+# test      
+# A=[1, 20, 6, 4, 5]        
+# B, n = sortAndCountInv(A)
+# print( B, n)
+#output 
+#[1, 4, 5, 6, 20] 5
+arr=[]
+with open("inversion.txt", "r") as fichiertext:
+    for ligne in fichiertext:
+        A=ligne.split()
+        a =int (A[0])
+        arr.append(a)        
+        
+B, n = sortAndCountInv(arr)
+print('number of inversions', n)
